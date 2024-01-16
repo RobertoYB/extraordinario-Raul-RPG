aa266bc0a829c7e673bed0593504e29ea9cde383
RobertoYB <ryupit26@gmail.com>
2024-01-16 07:36:37 -0600

    Creacion de interfaces principales
    Cree las dos interfaces principales de IHabilidad y IPersonaje. Agregue los atributos base de ambas interfaces ya que ahora agregaré las funciones que ejerce cada interfaz.

---
+++ Interfaces/IHabilidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Raul
+{
+    public interface IHabilidad
+    {
+        string id { get; }
+        string nombre { get; set; }
+        int nivelRequerido { get; set; }
+        int clase { get; }
+        int claseDelPersonajeAsociado { get; set; }
+    }
+}
---
+++ Interfaces/IPersonaje.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Raul.Interfaces
+{
+    public interface IPersonaje
+    {
+        string nombre { get; set; }
+        int nivel { set; }
+        int ataque { set; }
+        int defensa { set; }
+        int vidaMaxima { set; }
+        int vidaActual { get; set; }
+        int manaMaximo { set; }
+        int manaActual { get; set; }
+    }
+}
+
