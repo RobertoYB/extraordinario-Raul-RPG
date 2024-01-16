fe89096980ad91a60e6198a5b4dcf9c427091f9f
RobertoYB <146978884+RobertoYB@users.noreply.github.com>
2024-01-16 07:09:36 -0600

    Agregar archivos esenciales del proyecto
    Agregue los archivos iniciales del trabajo como el bin, obj y sln

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPG_Raul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
