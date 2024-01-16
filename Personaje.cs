d0ab194dc8fe59fce2f8ae2c8f9a5f4a085d8ac4
RobertoYB <ryupit26@gmail.com>
2024-01-16 08:54:14 -0600

    Funcion creacion de personaje
    Definí la funcion de crear personaje dentro de la clase personaje aplicando los atributos de la interfaz

--- Personaje.cs
+++ Personaje.cs
@@ -8,5 +8,81 @@ namespace RPG_Raul
 {
     public class Personaje : IPersonaje
     {
+        public string nombre;
+        private int nivel;
+        private int ataque;
+        private int defensa;
+        private int vidaMaxima;
+        private int vidaActual;
+        private int manaMaximo;
+        private int manaActual;
+
+        public void SubirNivel()
+        {
+
+        }
+
+        public void ObtenerHabilidadAprendida()
+        {
+
+        }
+        
+        public void ObtenerHabilidadEquipada()
+        {
+
+        }
+        
+        public void EquiparHabilidad()
+        {
+
+        }
+        
+        public void DesequiparHabilidad()
+        {
+
+        }
+        
+        public void LanzarHabilidad()
+        {
+
+        }
+        
+        public void Examinar()
+        {
+
+        }
+        
+        private void Morir()
+        {
+
+        }
+        
+        public void CreacionDePersonaje()
+        {
+            this.nombre = Console.ReadLine();
+            this.nivel = 1;
+
+            if (ClasePersonaje = ClasePersonaje.Guerrero)
+            {
+                this.ataque = 15;
+                this.defensa = 20;
+                this.vidaMaxima = 20;
+                this.manaMaximo = 30;
+            } 
+            else if (ClasePersonaje = ClasePersonaje.Mago)
+            {
+                this.ataque = 20;
+                this.defensa = 10;
+                this.vidaMaxima = 20;
+                this.manaMaximo = 30;
+            } 
+            else    //En este caso serian los datos del ladron
+            {
+                this.ataque = 10; 
+                this.defensa = 20;
+                this.vidaMaxima = 20;
+                this.manaMaximo = 15;
+            }
+        }
     }
 }
